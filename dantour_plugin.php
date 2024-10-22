5a501d0586424a1caf8cede321c91977d90d1541
twindai <guandai@hotmail.com>
2024-10-22 02:29:20 +0200

    add  data

--- dantour_plugin.php
+++ dantour_plugin.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: My Custom Footer
-Plugin URI: http://yourwebsite.com/my-custom-footer
+Plugin Name: Dantour Plugin
+Plugin URI: http://twindai.com/my-custom-footer
 Description: Adds a custom message to the footer of every post.
 Version: 1.0
-Author: Your Name
-Author URI: http://yourwebsite.com
+Author: twindai
+Author URI: http://twindai.com
 */
 
 // Hook into the 'the_content' filter
---
+++ tools/bash_wp_cli_update.sh
@@ -0,0 +1,26 @@
+; a:7:{i:0;a:5:{s:5:"label";s:5:"Day01";s:5:"title";s:16:"国内/奥斯陆";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:169:"<p>搭乘国际航班，飞往欧洲，抵达奥斯陆后，安排接机送酒店。</p> <p>调整时差，为进入接下来进入北极圈做好准备。</p> <p> </p>";}i:1;a:5:{s:5:"label";s:5:"Day02";s:5:"title";s:32:"奥斯陆-莱克内斯-Å-雷讷";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:665:"<p>早餐后前往机场，乘机经Bodo飞往位于北极圈内罗弗敦群岛中部城市Leknes。</p> <p><strong>参考航班</strong>: SK4106 奥斯陆-Bodo 10:45-12:10</p> <p><strong>参考航班</strong>: WF812 Bodo-Leknes 14:20-14:45</p> <p><b> </b></p> <p>到达Leknes后，乘车前往8公里外的Haukland Beach游览。随后经E10公路向西南行驶，沿途尽揽风光。行驶约70多公里后到达E10公路尽头，罗弗敦群岛最西南端城镇——Å。</p> <p>参观鳕鱼博物馆，在Å寄明信片。</p> <p>游览后入住最美渔村雷讷小镇（Reine），此地光污染很小，晚间若天气晴好，等待极光降临。</p>";}i:2;a:5:{s:5:"label";s:5:"Day03";s:5:"title";s:12:"雷讷小镇";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:716:"<p>上午：渔船出海钓鳕鱼之旅， 有机会看到鱼鹰、海豚等动物。全部航程4个小时。近距离了解群岛上渔民的日常生活，学习穿鱼线、撒鱼饵。有机会捕捉到鳕鱼、狼鱼、象牙鱼等；如果天气和运气良好，可能看到海鹰、海豹、鸬鹚，甚至鲸鱼。午间享受鳕鱼大餐。</p> <p>下午：Reinebringen Mountain专业徒步, 在当地专业徒步达人带领下，沿Reinebringen Mountain专业徒步线路。 山路较为艰难，但山顶风光绝美，俯瞰壮阔的峡湾渔村风景。 往返大约3-4小时。 海拔：448米，时长：2小时。</p> <p>晚上：视天气情况，可以在小镇周边观赏极光。</p> <p> </p>";}i:3;a:5:{s:5:"label";s:5:"Day04";s:5:"title";s:28:"雷讷小镇-斯沃尔韦尔";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:583:"<p>早餐后从Reine出发，向罗弗敦群岛北部前进。途经Ramberg的“北极圈内白沙滩”。参观著名的维京海盗博物馆*。来到老城卡伯尔沃格，参观挪威第二大的木质教堂和罗弗敦博物馆，了解罗弗敦群岛的历史。</p> <p>下午前往斯沃尔韦尔游览， 参观北挪威艺术中心。</p> <p>晚餐后，码头登上海达路德（Hurtigruten）游轮，前往北极圈内最大城市特罗姆瑟。天气晴朗时，在游轮甲板上可以尽情欣赏到极光。</p> <p><strong>住宿</strong>: 游轮上</p> <p> </p>";}i:4;a:5:{s:5:"label";s:5:"Day05";s:5:"title";s:12:"特罗姆瑟";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:289:"<p>上午：游轮自由活动，观光</p> <p>下午：抵达特罗姆瑟港口， 14:30，乘坐缆车到达斯托恩山顶，俯瞰特罗姆瑟全景。</p> <p>晚上：极光拍摄， 视天气及太阳活动情况，安排大家在城市周边峡湾地区拍摄极光。</p> <p> </p>";}i:5;a:5:{s:5:"label";s:5:"Day06";s:5:"title";s:12:"特罗姆瑟";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:777:"<p>上午：出海观鲸， 酒店早餐后，前往市中心搭乘温暖舒适的双体船出海观鲸。可能看到虎鲸、座头鲸、长须鲸等。船程4-6小时，提供咖啡、热茶、巧克力及茶点。专业船长讲解观鲸规则和安全事项，准备好相机。</p> <p>下午：参观北极大教堂（Arctic Cathedral）， 参观雪山造型的北极大教堂，拥有北欧最大的彩色玻璃窗，建于1965年。</p> <p>之后前往马克啤酒屋， 前往世界最北的酿酒厂——马克啤酒屋。了解Mark啤酒的历史和酿造过程。在电脑上输入自己的名字，制造一瓶以自己命名的啤酒。</p> <p>晚上：极光拍摄 视天气及太阳活动情况，安排在城市周边峡湾地区拍摄极光。</p> <p> </p>";}i:6;a:5:{s:5:"label";s:5:"Day07";s:5:"title";s:29:"特罗姆瑟/奥斯陆/国内";s:4:"date";s:12:"Invalid date";s:4:"time";s:0:"";s:4:"desc";s:281:"<p><b> <strong>参考航班</strong>: SK4419 特罗姆瑟-奥斯陆 12:40-14:30</b></p> <p>酒店早餐后，统一前往机场搭乘航班返回奥斯陆。</p> <p>之后经过奥斯陆转机返回各团友出发城市，结束本次经典的北挪威行摄旅行。</p> <p> </p>";}}
+
+
+; array (
+;   array (
+;     'label' => 'Day01',
+;     'title' => '国内/奥斯陆',
+;     'date' => 'Invalid date',
+;     'time' => '',
+;     'desc' => 'aa',
+;   ),
+; )
+
+
+; wp post meta update 2970 wp_travel_trip_itinerary_data '[{"label":"Day01","title":"国内/奥斯陆","date":"Invalid date","time":"","desc":"aa"}]'  --format=json
+; wp post meta get 2970 wp_travel_trip_itinerary_data  --format==json
+; [{"label":"Day01","title":"\u56fd\u5185\/\u5965\u65af\u9646","date":"Invalid date","time":"","desc":"aa"}]
+
+
+post_ids=$(wp post list --post_type=itineraries --fields=ID)
+for i in $post_ids; do
+		echo $(wp post meta get $i _thumbnail_id --format=json)
+    existing_data=$(wp post meta get $i wp_travel_trip_itinerary_data --format=json)
+		; modified_data=$(echo "$existing_data" | jq 'map(.date = "")')
+done
+
