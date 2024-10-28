b40001fb7bbf6f477dd8b24ef44e78c89a29e918
twindai <guandai@hotmail.com>
2024-10-28 18:17:00 +0100

    add

--- index.php
+++ index.php
@@ -38,4 +38,8 @@ function insert_js_by_url_name($file_name, $path_contains) {
 insert_js_by_url_name('form_change_upload_file', '/book_');
 insert_js_by_url_name('form_load_wpuserdata', '/book_');
 insert_js_by_url_name('form_add_repeater', '/book_');
+insert_js_by_url_name('form_vehicel_active', '/book_');
+
 insert_js_by_url_name('form_show_sent_modal', '/booking_');
+insert_js_by_url_name('global_navigation', '/');
+insert_js_by_url_name('account_order_data', '/account');
